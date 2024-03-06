ac1c7d7281480961a5261e7df6bdbae6c42bc039
cancio <filipe.cancio@gmail.com>
2024-03-06 19:56:33 -0300

    criando tela home

---
+++ lib/components/atom/product_item.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/widgets.dart';
+
+class ProductItem extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ProductItemState();
+  }
+
+}
+
+class ProductItemState extends State<ProductItem>{
+  @override
+  Widget build(BuildContext context) {
+    return const Text("produto");
+  }
+
+}
